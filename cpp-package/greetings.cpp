fb318c8800d4b185215992ef04c415ece6fd9332
sameagen-MW <sameagen@mathworks.com>
2025-06-12 16:44:52 -0400

    Library content

---
+++ cpp-package/greetings.cpp
@@ -0,0 +1,9 @@
+#include "greetings.h"
+#include <iostream>
+
+using namespace greetings;
+using namespace std;
+
+void greet(string name) {
+    cout << "Hello " << name << "!";
+}
---
+++ cpp-package/greetings.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+namespace greetings {
+    void greet(string name);
+}
